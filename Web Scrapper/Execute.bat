5c4e25eed38b2edf6cc92c47d8f5515469400774
MBrcz <mat.borcz123@gmail.com>
2024-01-28 15:21:25 +0100

    Updated the project 28/01/2024

--- Web Scrapper/Execute.bat
+++ Web Scrapper/Execute.bat
@@ -20,19 +20,3 @@ call python main.py
 ::Deactivating venv
 echo Deactivation of venv
 call %venv_deactivate%
-
-:: Question Section.
-setlocal enabledelayedexpansion
-
-:ask_question
-set /p "response=Do you want to open the report file? Do not forget to refresh (Y/N): "
-if /i "!response!"=="Y" (
-    echo The report file has been opened.
-    cd /d ..\Report
-    start "Report File" "Oryx Report.pbix"
-) else if /i "!response!"=="N" (
-    echo Fine, have a nice day!
-) else (
-    echo Please enter 'Y' or 'N'.
-    goto ask_question
-)
